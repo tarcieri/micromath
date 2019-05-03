9dd016633f61688ab530cfd7da7717c24005359b
Tony Arcieri <bascule@gmail.com>
2019-05-03 12:31:09 -0700

    f32ext: invsqrt(x)
    Implements fast inverse square root using the method described at:
    
    https://bits.stephan-brumme.com/invSquareRoot.html

---
+++ src/f32ext/invsqrt.rs
@@ -0,0 +1,34 @@
+/// Inverse square root approximation function for a single-precision float.
+/// Method described at: <https://bits.stephan-brumme.com/invSquareRoot.html>
+pub(super) fn invsqrt_approx(x: f32) -> f32 {
+    f32::from_bits(0x5F37_5A86 - (x.to_bits() >> 1))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::invsqrt_approx;
+    use crate::f32ext::sqrt::tests::TEST_VECTORS;
+
+    /// Maximum error as a percentage of the input value (5%)
+    const MAX_ERROR: f32 = 0.05;
+
+    #[test]
+    fn sanity_check() {
+        for (x, expected) in TEST_VECTORS {
+            // The tests vectors are for sqrt(x), so invert the expected value
+            let expected = 1.0 / expected;
+
+            let invsqrt_x = invsqrt_approx(*x);
+            let allowed_delta = x * MAX_ERROR;
+            let actual_delta = invsqrt_x - expected;
+
+            assert!(
+                actual_delta <= allowed_delta,
+                "delta {} too large: {} vs {}",
+                actual_delta,
+                invsqrt_x,
+                expected
+            );
+        }
+    }
+}
--- src/f32ext/sqrt.rs
+++ src/f32ext/sqrt.rs
@@ -1,44 +1,42 @@
 /// Square root approximation function for a single-precision float.
 /// Method described at: <https://bits.stephan-brumme.com/squareRoot.html>
-pub(super) fn sqrt_approx(n: f32) -> f32 {
-    let mut n = n.to_bits();
-    n += 127 << 23;
-    n >>= 1;
-    f32::from_bits(n)
+pub(super) fn sqrt_approx(x: f32) -> f32 {
+    f32::from_bits((x.to_bits() + 0b11_1111_1000_0000_0000_0000_0000_0000) >> 1)
 }
 
 #[cfg(test)]
-mod tests {
+pub(super) mod tests {
     use super::sqrt_approx;
 
     /// Maximum error as a percentage of the input value (5%)
-    const MAX_ERROR: f32 = 0.05;
+    pub(crate) const MAX_ERROR: f32 = 0.05;
+
+    /// Square root test vectors
+    pub(crate) const TEST_VECTORS: &[(f32, f32)] = &[
+        (1.0, 1.0),
+        (2.0, 1.414),
+        (3.0, 1.732),
+        (4.0, 2.0),
+        (5.0, 2.236),
+        (10.0, 3.162),
+        (100.0, 10.0),
+        (250.0, 15.811),
+        (500.0, 22.36),
+        (1000.0, 31.622),
+        (2500.0, 50.0),
+        (5000.0, 70.710),
+        (1000000.0, 1000.0),
+        (2500000.0, 1581.138),
+        (5000000.0, 2236.067),
+        (10000000.0, 3162.277),
+        (25000000.0, 5000.0),
+        (50000000.0, 7071.067),
+        (100000000.0, 10000.0),
+    ];
 
     #[test]
     fn sanity_check() {
-        let sqrt_test_vectors = [
-            (1.0, 1.0),
-            (2.0, 1.414),
-            (3.0, 1.732),
-            (4.0, 2.0),
-            (5.0, 2.236),
-            (10.0, 3.162),
-            (100.0, 10.0),
-            (250.0, 15.811),
-            (500.0, 22.36),
-            (1000.0, 31.622),
-            (2500.0, 50.0),
-            (5000.0, 70.710),
-            (1000000.0, 1000.0),
-            (2500000.0, 1581.138),
-            (5000000.0, 2236.067),
-            (10000000.0, 3162.277),
-            (25000000.0, 5000.0),
-            (50000000.0, 7071.067),
-            (100000000.0, 10000.0),
-        ];
-
-        for (x, expected) in &sqrt_test_vectors {
+        for (x, expected) in TEST_VECTORS {
             let sqrt_x = sqrt_approx(*x);
             let allowed_delta = x * MAX_ERROR;
             let actual_delta = sqrt_x - expected;
