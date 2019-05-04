c0570aff1a0fd8eebd0fb6eab8dc680ee72f60bb
Tony Arcieri <bascule@gmail.com>
2019-05-04 09:32:28 -0700

    statistics: Add Trim trait for statistical outlier culling iterators

--- src/statistics.rs
+++ src/statistics.rs
@@ -3,16 +3,19 @@
 //! The following traits are available and impl'd for slices and iterators of
 //! `f32` (and can be impl'd for other types):
 //!
-//! - [Mean] - compute arithmetic mean with the `mean()` method
+//! - [Mean] - compute arithmetic mean with the `mean()` method.
 //! - [StdDev] - compute standard deviation with the `stddev()` method
-//! - [Variance] - compute variance with the `variance() method
+//! - [Trim] - cull outliers from a sample slice with the `trim()` method.
+//! - [Variance] - compute variance with the `variance() method.
 //!
 //! [Mean]: https://docs.rs/micromath/latest/micromath/statistics/trait.Mean.html
 //! [StdDev]: https://docs.rs/micromath/latest/micromath/statistics/trait.StdDev.html
+//! [Trim]: https://docs.rs/micromath/latest/micromath/statistics/trait.Trim.html
 //! [Variance]: https://docs.rs/micromath/latest/micromath/statistics/trait.Variance.html
 
 mod mean;
 mod stddev;
+pub mod trim;
 mod variance;
 
-pub use self::{mean::Mean, stddev::StdDev, variance::Variance};
+pub use self::{mean::Mean, stddev::StdDev, trim::Trim, variance::Variance};
