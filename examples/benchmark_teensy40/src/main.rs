f0054b3c04014a7737df82a0e1e7075cf6bdc491
Finomnis <finomnis@gmail.com>
2023-05-31 16:02:27 +0200

    Add time measurement

--- examples/benchmark_teensy40/src/main.rs
+++ examples/benchmark_teensy40/src/main.rs
@@ -17,21 +17,45 @@ use teensy4_bsp as bsp;
 use teensy4_panic as _;
 
 use bsp::board;
+use bsp::hal;
 
 mod uart_writer;
 use uart_writer::UartWriter;
 
 #[bsp::rt::entry]
 fn main() -> ! {
-    let board::Resources { pins, lpuart2, .. } = board::t40(board::instances());
+    let board::Resources {
+        pins,
+        mut gpio2,
+        lpuart2,
+        gpt1: mut us_timer,
+        ..
+    } = board::t40(board::instances());
 
-    // Initialize uart
+    // Initialize LED
+    let led = board::led(&mut gpio2, pins.p13);
+    led.set();
+
+    // Initialize UART
     let mut uart = UartWriter::new(board::lpuart(lpuart2, pins.p14, pins.p15, 115200));
     writeln!(uart);
+
+    // Initialize timer
+    // Is a 32-bit timer with us precision.
+    // Overflows every 71.58 minutes, which is sufficient for our benchmark.
+    assert_eq!(board::PERCLK_FREQUENCY, 1_000_000);
+    us_timer.set_clock_source(hal::gpt::ClockSource::PeripheralClock);
+    us_timer.set_divider(1);
+    us_timer.set_mode(hal::gpt::Mode::FreeRunning);
+    us_timer.enable();
 
     // Write welcome message
     writeln!(uart, "===== Micromath Benchmark =====");
     writeln!(uart, "Git Version: {}", git_version::git_version!());
+    writeln!(uart);
+
+    writeln!(uart, "Time: {}", us_timer.count());
+    writeln!(uart, "Time: {}", us_timer.count());
 
     loop {}
 }
